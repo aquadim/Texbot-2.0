f302aec42da7ed2e60033616324fef4f2de3a94b
Вадим Королёв <pydim@mail.ru>
2025-02-05 15:04:13 +0300

    Исправлена ошибка в исключении

--- botkit/Exceptions/DatabaseException.php
+++ botkit/Exceptions/DatabaseException.php
@@ -2,7 +2,7 @@
 // Исключение, связанное с базой данных.
 namespace BotKit\Exceptions;
 
-class DatabaseException {
+class DatabaseException extends \Exception {
     public function __construct($message) {
         parent::__construct($message, 0, null);
     }
